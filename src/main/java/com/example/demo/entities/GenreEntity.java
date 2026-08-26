a7805ea7929408e59688188451fce465382cf3c8
Hanna <hannafrolova14@gmail.com>
2026-08-26 18:44:20 +0200

    feat: add Movie Genre relationship

--- src/main/java/com/example/demo/entities/GenreEntity.java
+++ src/main/java/com/example/demo/entities/GenreEntity.java
@@ -1,19 +1,28 @@
 package com.example.demo.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import java.util.Set;
 
 @Entity
 @Table(name = "genres")
 public class GenreEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idGenre;
 
     private String genre;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "genres")
+    private Set<MovieEntity> movies;
 
     public GenreEntity() {
     }
