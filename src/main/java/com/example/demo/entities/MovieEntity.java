add92d184950b785878210e53a8b1c9fd0ef5169
Hanna <hannafrolova14@gmail.com>
2026-08-26 18:27:00 +0200

    feat: add Movie entity relationships

---
+++ src/main/java/com/example/demo/entities/MovieEntity.java
@@ -0,0 +1,75 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "movies")
+public class MovieEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idMovie;
+
+    private String movieName;
+
+    @ManyToOne
+    @JoinColumn(name = "year_id")
+    private YearEntity year;
+
+    @ManyToMany
+    @JoinTable(
+        name = "movie_genre",
+        joinColumns = @JoinColumn(name = "movie_id"),
+        inverseJoinColumns = @JoinColumn(name = "genre_id")
+    )
+
+    private Set<GenreEntity> genres;
+
+    public MovieEntity() {
+    }
+
+    public MovieEntity(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public Long getIdMovie() {
+        return idMovie;
+    }
+
+    public void setIdMovie(Long idMovie) {
+        this.idMovie = idMovie;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public YearEntity getYear() {
+        return year;
+    }
+
+    public void setYear(YearEntity year) {
+        this.year = year;
+    }
+
+    public Set<GenreEntity> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(Set<GenreEntity> genres) {
+        this.genres = genres;
+    }
+}
