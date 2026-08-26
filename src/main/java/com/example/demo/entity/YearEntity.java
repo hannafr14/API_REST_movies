5ba9cb67fcb3c3ab0d04720b0c681b5e3a2a079e
Hanna <hannafrolova14@gmail.com>
2026-08-26 09:42:01 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/entity/YearEntity.java
@@ -0,0 +1,41 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "years")
+public class YearEntity {
+    
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long idYear;
+
+    private Integer year;
+
+    public YearEntity() {
+    }
+
+    public YearEntity(Integer year) {
+        this.year = year;
+    }
+
+    public Long getIdYear() {
+        return idYear;
+    }
+
+    public void setIdYear(Long idYear) {
+        this.idYear = idYear;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+}
