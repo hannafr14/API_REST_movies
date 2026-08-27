522eaeca3ed4851b0d456d47015df9d6c4ee6207
Hanna <hannafrolova14@gmail.com>
2026-08-27 09:28:36 +0200

    feat: add movie service interface

---
+++ src/main/java/com/example/demo/services/InterfaceMovieService.java
@@ -0,0 +1,8 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.MovieEntity;
+import java.util.List;
+
+public interface InterfaceMovieService {
+    List<MovieEntity> getAllMovies();
+}
